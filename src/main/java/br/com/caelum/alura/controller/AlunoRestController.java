1132a8c3c392f1692b883fe01a6d27de8a766130
alexfelipe <alexfelipevieira@gmail.com>
2016-07-21 10:20:32 -0300

    remoção do diretório .mvn

--- src/main/java/br/com/caelum/alura/controller/AlunoRestController.java
+++ src/main/java/br/com/caelum/alura/controller/AlunoRestController.java
@@ -3,6 +3,9 @@ package br.com.caelum.alura.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.caelum.alura.model.Aluno;
 import br.com.caelum.alura.service.AlunoService;
+import static br.com.caelum.alura.utils.HTTPValues.JSON;
 
 @RestController
 @RequestMapping("v1/aluno")
@@ -25,6 +29,12 @@ public class AlunoRestController {
 	@RequestMapping(method = RequestMethod.GET)
 	public @ResponseBody List<Aluno> alunos() {
 		return alunoService.getLista();
+	}
+
+	@RequestMapping(method = RequestMethod.POST, consumes = JSON)
+	public ResponseEntity<String> inserir(@RequestBody Aluno aluno) {
+		alunoService.salvar(aluno);
+		return new ResponseEntity<>("aluno cadastrado", HttpStatus.OK);
 	}
 
 }
